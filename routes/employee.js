eb374f5ae8a82a4633220a57ac1ccdb8e24f63bc
nikk2706 <137515605+nikk2706@users.noreply.github.com>
2023-10-05 11:10:37 +0530

    Add files via upload

---
+++ routes/employee.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const {
+    createEmployeeControl,
+    getAllEmployeeControl,
+    getEmployeeByIdControl,
+    updateEmployeeByIdcontrol,
+    destroy,
+    deleteEmployeeByIdcontrol
+} = require('../controllers/EmployeeController')
+
+const router = express.Router()
+
+router.post('/', createEmployeeControl)
+router.get('/', getAllEmployeeControl)
+router.get('/:id', getEmployeeByIdControl)
+router.put('/:id', updateEmployeeByIdcontrol)
+router.delete('/:id', deleteEmployeeByIdcontrol)
+
+
+
+module.exports = router
